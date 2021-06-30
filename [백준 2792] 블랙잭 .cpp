23e59af04451e986ba1483540bbd29060c36dd96
이대영 <tom910903@gmail.com>
2021-06-30 22:07:52 +0900

    [백준 2792] 블랙잭

---
+++ [백준 2792] 블랙잭 .cpp
@@ -0,0 +1,51 @@
+﻿#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int N, M;
+int val = 0;
+vector<int> card;
+int bin[5];
+bool use_card[150000] = { false, };
+
+void tobin(int length) {
+    if (length >= 3) {
+        int t_val = bin[0] + bin[1] + bin[2];
+        // for(int i = 0 ; i < 3 ; i++){
+        //     cout << bin[i] << " ";
+        // }
+        // cout << endl;
+
+        if (t_val > val && t_val <= M) {
+            val = t_val;
+        }
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (!use_card[card[i]]) {
+            bin[length] = card[i];
+            use_card[card[i]] = true;
+            tobin(length + 1);
+            use_card[card[i]] = false;
+        }
+    }
+}
+
+int main() {
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        int num;
+        cin >> num;
+        card.push_back(num);
+    }
+
+    tobin(0);
+
+    cout << val;
+
+    return 0;
+}
