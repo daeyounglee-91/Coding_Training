db206139d7f01e9152493b2076c3a2e01145153e
이대영 <tom910903@gmail.com>
2021-07-01 20:44:44 +0900

    [백준 2231] 분해합

---
+++ [백준 2231] 분해합.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int check(int num) {
+    int temp = num;
+    int sum = num;
+    while (temp)
+    {
+        sum += temp % 10;
+        temp /= 10;
+    }
+    return sum;
+}
+
+int main() {
+
+    int N;
+
+    cin >> N;
+
+    for (int i = 0; i < 1000000; i++) {
+        if (N == check(i)) {
+            printf("%d", i);
+            return 0;
+        }
+    }
+
+    printf("0");
+
+    return 0;
+}
