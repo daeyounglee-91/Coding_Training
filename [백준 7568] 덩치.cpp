c81444073fc85a4f9359204b04bb6088c31db1fe
이대영 <tom910903@gmail.com>
2021-07-03 20:26:22 +0900

    [백준 7568] 덩치

---
+++ [백준 7568] 덩치.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+    int N;
+    vector<pair<int, int>> v;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int w, h;
+        cin >> w >> h;
+        v.push_back(make_pair(w, h));
+    }
+
+    for (int i = 0; i < N; i++) {
+        int num = 1;
+        for (int j = 0; j < N; j++) {
+            if (i == j) continue;
+            if (v[i].first < v[j].first && v[i].second < v[j].second) {
+                num++;
+            }
+        }
+        printf("%d ", num);
+    }
+
+    return 0;
+}
