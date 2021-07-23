a76312e1207b315d92625031e8d29d9e4259a540
이대영 <tom910903@gmail.com>
2021-07-23 21:30:31 +0900

    [완전탐색] 모의고사

--- 프로그래머스/[완전탐색] 모의고사.py
+++ 프로그래머스/[완전탐색] 모의고사.py
@@ -1,10 +1,24 @@
-def solution(array, commands):
+def solution(answers):
     answer = []
+    arr = [0,0,0]
+    max = 0
 
-    for cmd in commands:
-        print("0 : {0} , 1 : {1}".format(cmd[0],cmd[1]))
-        temp = array[cmd[0]-1:cmd[1]]
-        temp.sort()
-        answer.append(temp[cmd[2]-1])
+    answer1 = [1,2,3,4,5]
+    answer2 = [2,1,2,3,2,4,2,5]
+    answer3 = [3,3,1,1,2,2,4,4,5,5]
+
+    for i in range(0,len(answers)):
+        if answer1[i%len(answer1)] == answers[i]:
+            arr[0] += 1
+            if max < arr[0] : max = arr[0]
+        if answer2[i%len(answer2)] == answers[i]:
+            arr[1] += 1
+            if max < arr[1] : max = arr[1]
+        if answer3[i%len(answer3)] == answers[i]:
+            arr[2] += 1
+            if max < arr[2] : max = arr[2]
+
+    for i in range(0,len(arr)):
+        if max == arr[i]: answer.append(i+1)
 
     return answer
