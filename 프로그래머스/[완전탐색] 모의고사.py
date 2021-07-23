e2d2a2bc4206e096cff07c91e5a7abd459f03fa9
이대영 <tom910903@gmail.com>
2021-07-23 21:23:12 +0900

    [완전탐색] 모의고사

---
+++ 프로그래머스/[완전탐색] 모의고사.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+
+    for cmd in commands:
+        print("0 : {0} , 1 : {1}".format(cmd[0],cmd[1]))
+        temp = array[cmd[0]-1:cmd[1]]
+        temp.sort()
+        answer.append(temp[cmd[2]-1])
+
+    return answer
