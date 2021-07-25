03bc2bc51bc09c2c01a76440d5f95391bc501ed7
이대영 <tom910903@gmail.com>
2021-07-25 14:26:46 +0900

    [해시] 완주하지 못한 선수

---
+++ 프로그래머스/[해시] 완주하지 못한 선수.py
@@ -0,0 +1,11 @@
+
+def solution(participant, completion):
+    
+    participant = sorted(participant)
+    completion = sorted(completion)
+
+    for i in range(0,len(completion)):
+        if(participant[i] != completion[i]) :return participant[i]
+
+
+    return participant[len(participant)-1]
