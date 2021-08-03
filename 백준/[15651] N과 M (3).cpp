d2801c61f102566e76cdf26288bb29b95f01e2f0
이대영 <tom910903@gmail.com>
2021-08-03 20:05:39 +0900

    [15651] N과 M (3)

---
+++ 백준/[15651] N과 M (3).cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+int M,N;
+int bin[10] = {0,};
+
+void back_tracking(int length){
+
+    if(length == M){
+        for(int i = 0 ; i < M ; i++){
+            printf("%d ", bin[i]);
+        }
+        printf("\n");
+        return;
+    }
+
+    for(int i = 1 ; i <= N ; i++){
+        bin[length] = i; 
+        back_tracking(length+1);
+    }
+}
+
+int main() {
+
+    scanf("%d %d", &N, &M);
+
+    back_tracking(0);
+
+	return 0;
+}
