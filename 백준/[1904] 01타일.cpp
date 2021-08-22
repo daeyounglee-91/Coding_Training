22c86466e925ef37471d0189758d1fc96014b7fd
이대영 <tom910903@gmail.com>
2021-08-22 20:43:23 +0900

    [1904] 01타일

---
+++ 백준/[1904] 01타일.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int arr[1500000] ={0,};
+int N;
+
+int main(){
+	
+
+	arr[0] = 1;
+	arr[1] = 1;
+	cin >> N;
+
+	if(N > 1){
+		for(int i = 2 ; i <= N ; i++){
+			arr[i] = (arr[i-2] + arr[i-1])%15746;
+		}
+	}
+	cout << arr[N];
+	return 0;
+}
