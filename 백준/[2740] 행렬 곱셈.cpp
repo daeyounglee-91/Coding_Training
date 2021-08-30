fd21b99c1753f10a6f9bafe506d064541ba19552
이대영 <tom910903@gmail.com>
2021-08-30 22:18:42 +0900

    [2740] 행렬 곱셈

---
+++ 백준/[2740] 행렬 곱셈.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int arr[3][150][150];
+int N, M, K;
+
+int main() {
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> arr[0][i][j];
+		}
+	}
+
+	cin >> M >> K;
+
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < K; j++) {
+			cin >> arr[1][i][j];
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < K; j++) {
+			for (int k = 0; k < M; k++) {
+				arr[2][i][j] += (arr[0][i][k] * arr[1][k][j]);
+			}
+		}
+	}
+
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < K; j++) {
+			cout << arr[2][i][j] << " ";
+		}
+		cout << endl;
+	}
+
+
+	return 0;
+}
